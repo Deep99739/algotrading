40f1e321cedfe18a79e7a7a5263a382f7bc2d511
Deep99739 <142610132+Deep99739@users.noreply.github.com>
2025-06-29 13:49:08 +0530

    Fix data indexing and paths

--- momentum.py
+++ momentum.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import numpy as np
 import datetime
@@ -12,29 +13,47 @@ F = 0.005
 D = 500
 B = 10000
 
-def GetData(NameOfFile):
-  return pd.DataFrame(pd.read_csv(NameOfFile, usecols = ['datadate','tic','adjcp']))
+def GetData(file_path: str) -> pd.DataFrame:
+    """Load the price dataset with only the required columns."""
+    return pd.read_csv(file_path, usecols=["datadate", "tic", "adjcp"])
 
-def PartitionData(Data):
-  DateToIndex = {}
-  for e in range(len(Data['datadate'])):
-    DateToIndex.setdefault(str(Data['datadate'][e]),e)
-  return [np.array_split(Data,2926), DateToIndex]
+def PartitionData(data: pd.DataFrame):
+    """Split the dataframe by date and create an index mapping."""
+    date_to_index = {}
+    for idx, date in enumerate(data["datadate"]):
+        date_to_index.setdefault(str(date), idx)
+    # Each day consists of 30 consecutive rows
+    return [np.array_split(data, 2926), date_to_index]
 
-def GetMomentumBasedPriority(PartitionedDataFrameList, DateToIndex ,today):
-  NdaysAgo = datetime.date(int(today[0:4]),int(today[4:6]),int(today[6:])) + datetime.timedelta(days = -N)
-  i = 0
-  while i >= 0:
-    x = str(NdaysAgo - datetime.timedelta(days = i)).replace('-','')
-    if (x in DateToIndex.keys()):
-      break
-    i += 1
-  temp = x
-  Momentum = np.array(PartitionedDataFrameList[DateToIndex[today] % 2926]['adjcp']) - np.array(PartitionedDataFrameList[DateToIndex[temp] % 2926]['adjcp'])
-  Sum = np.array(PartitionedDataFrameList[DateToIndex[today] % 2926]['adjcp'])
-  for i in range((DateToIndex[temp] + 1), (DateToIndex[today]),30):
-    Sum += np.array(PartitionedDataFrameList[i % 2926]['adjcp'])
-  return Momentum * N / Sum
+def GetMomentumBasedPriority(partitioned_data, date_to_index, today):
+    """Calculate momentum weights for the given day."""
+    n_days_ago = (
+        datetime.date(int(today[0:4]), int(today[4:6]), int(today[6:]))
+        + datetime.timedelta(days=-N)
+    )
+
+    i = 0
+    while True:
+        candidate = str(n_days_ago - datetime.timedelta(days=i)).replace("-", "")
+        if candidate in date_to_index:
+            break
+        i += 1
+
+    temp = candidate
+
+    today_idx = date_to_index[today]
+    past_idx = date_to_index[temp]
+
+    momentum = (
+        np.array(partitioned_data[today_idx]["adjcp"])
+        - np.array(partitioned_data[past_idx]["adjcp"])
+    )
+
+    summation = np.array(partitioned_data[today_idx]["adjcp"], dtype=float)
+    for j in range(past_idx + 1, today_idx):
+        summation += np.array(partitioned_data[j]["adjcp"], dtype=float)
+
+    return momentum * N / summation
 
 def GetBalanced(prices, weights, balance):
   copy = np.flip(np.sort(weights))
@@ -53,52 +72,63 @@ def GetBalanced(prices, weights, balance):
   return (balance / sum) * weights
 
 class PortFolio:
-  def __init__(self, balance, numStocks, prices):
-    self.balance = balance
-    self.numStocks = numStocks
-    self.prices = prices
+    def __init__(self, balance, num_stocks, prices):
+        self.balance = balance
+        self.numStocks = num_stocks
+        self.prices = prices
 
-  def SellStock(self, index):
-    self.balance += self.numStocks[index] * self.prices[index] * (1 - F)
-    self.numStocks[index] = 0
-  
-  def BuyStock(self, index, number):
-    self.balance -= number * self.prices[index] * (1 + F)
+    def SellStock(self, index):
+        self.balance += self.numStocks[index] * self.prices[index] * (1 - F)
+        self.numStocks[index] = 0
 
-  def CalculateNetWorth(self):
-    return self.balance + np.sum(self.numStocks * self.prices) * (1 - F)
+    def BuyStock(self, index, number):
+        self.balance -= number * self.prices[index] * (1 + F)
 
-  def ChangePricesTo(self, newPriceVector):
-    self.prices = newPriceVector
+    def CalculateNetWorth(self):
+        return self.balance + np.sum(self.numStocks * self.prices) * (1 - F)
 
-  def RebalancePortFolio(self, newWeights):
-    balanceCopy = self.balance + np.sum(self.numStocks * self.prices) * (1 - F)
-    newStocks = GetBalanced(self.prices, newWeights, balanceCopy)
-    for i in range(30):
-      balanceCopy -= self.prices[i] * newStocks[i] * (1 + F)
-    if balanceCopy + np.sum(self.prices * newStocks) * (1 - F) + B * (1 - R) >= self.CalculateNetWorth():
-      self.balance = balanceCopy
-      self.numStocks = newStocks
+    def ChangePricesTo(self, newPriceVector):
+        self.prices = newPriceVector
+
+    def RebalancePortFolio(self, newWeights):
+        balanceCopy = self.balance + np.sum(self.numStocks * self.prices) * (1 - F)
+        newStocks = GetBalanced(self.prices, newWeights, balanceCopy)
+        for i in range(30):
+            balanceCopy -= self.prices[i] * newStocks[i] * (1 + F)
+        if (
+            balanceCopy + np.sum(self.prices * newStocks) * (1 - F) + B * (1 - R)
+            >= self.CalculateNetWorth()
+        ):
+            self.balance = balanceCopy
+            self.numStocks = newStocks
 
 def VisualizeData(FinalData):
   plt.plot(FinalData)
   plt.show()
 
-Data = GetData("/Users/deepakkumar/desktop/algo-trading-main/DATA.csv")
+DATA_FILE = os.path.join(os.path.dirname(__file__), "DATA.csv")
+Data = GetData(DATA_FILE)
 List = PartitionData(Data)
 PartitionedData = List[0]
 DateToIndex = List[1]
 
-myPortfolio = PortFolio(B * R,np.zeros(30, dtype = float),np.array(PartitionedData[int(list(DateToIndex.keys())[N]) % 2926]['adjcp']))
+myPortfolio = PortFolio(
+    B * R,
+    np.zeros(30, dtype=float),
+    np.array(PartitionedData[N]["adjcp"]),
+)
 NetWorthAfterEachTrade = [myPortfolio.CalculateNetWorth() + B * (1 - R)]
 
-for i in range((N + 1),len(PartitionedData)):
-  today = list(DateToIndex.keys())[i]
-  myPortfolio.ChangePricesTo(np.array(PartitionedData[int(today) % 2926]['adjcp']))
-  NetWorthAfterEachTrade.append(myPortfolio.CalculateNetWorth() + B * (1 - R))
-  if (i % T == 0):
-    myPortfolio.RebalancePortFolio(GetMomentumBasedPriority(PartitionedData, DateToIndex, today))
-  if i == N + D + 6:
-    break
+for i in range(N + 1, len(PartitionedData)):
+    today = list(DateToIndex.keys())[i]
+    myPortfolio.ChangePricesTo(np.array(PartitionedData[i]["adjcp"]))
+    NetWorthAfterEachTrade.append(myPortfolio.CalculateNetWorth() + B * (1 - R))
+    if i % T == 0:
+        myPortfolio.RebalancePortFolio(
+            GetMomentumBasedPriority(PartitionedData, DateToIndex, today)
+        )
+    if i == N + D + 6:
+        break
 
 VisualizeData(NetWorthAfterEachTrade[:D])
+
